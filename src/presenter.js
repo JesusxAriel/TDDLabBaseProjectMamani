72d9d3e8beb543d3a02e048fda47aa3bc73cef17
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-02-12 15:40:11 -0400

    Multiplicador añadido

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,11 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
+// Suma
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const div = document.querySelector("#resultado-sumar-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +14,18 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+
+// Multiplicacion
+const multiplicarForm = document.querySelector("#multiplicar-form");
+const multiplicarDiv = document.querySelector("#resultado-multiplicar-div");
+
+multiplicarForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  multiplicarDiv.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
